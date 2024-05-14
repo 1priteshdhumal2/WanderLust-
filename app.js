77c0eecdb5d333412c2f019ef55fe61df676cd9f
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-05-14 21:59:33 +0530

    Added Edit & Update route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require('method-override');
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -11,6 +12,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
 
 main().then(() => {
     console.log("Connected to DB");
@@ -46,6 +48,20 @@ app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
+});
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+//Update route
+app.put("/listings/:id",async (req, res) => {
+    let {id} = req.params;
+    const list = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
 });
 
 app.listen(8080, () => {
