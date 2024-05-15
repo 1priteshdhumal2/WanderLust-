4eb6f974bf54145db19d9eb548300ed897abbf01
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-05-15 23:34:09 +0530

    Created Delete route and added NavBar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -13,6 +14,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 main().then(() => {
     console.log("Connected to DB");
@@ -63,6 +66,14 @@ app.put("/listings/:id",async (req, res) => {
     const list = await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
 });
+
+//Delete route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log("Server is listening to port 8080");
