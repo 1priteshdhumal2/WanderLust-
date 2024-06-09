7c3a76773553a6f3bcf4d9b203412045460449d5
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-06-09 23:13:52 +0530

    Validate and render reviews

--- app.js
+++ app.js
@@ -3,96 +3,130 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js');
-const ExpressError = require('./utils/ExpressError.js');
-const { listingSchema } = require('./schema.js');
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected to DB");
-})
-    .catch((err) => {
-        console.log(err);
-    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
-    res.send("Hi, I am root");
+  res.send("Hi, I am root");
 });
 
 const ValidateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(',');
-        throw new ExpressError(400, errMsg);
-    }
-    else {
-        next();
-    }
-}
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+const ValidateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
 //index Route
-app.get("/listings", wrapAsync(async (req, res) => {
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
     let allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-}));
+  })
+);
 
 //New Route
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
+  res.render("listings/new.ejs");
 });
 
 //show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
-}));
+  })
+);
 
 //Create Route
-app.post("/listings", ValidateListing, wrapAsync(async (req, res, next) => {
+app.post(
+  "/listings",
+  ValidateListing,
+  wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
     next(err);
-}));
+  })
+);
 
 //Edit route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-}));
+  })
+);
 
 //Update route
-app.put("/listings/:id", ValidateListing, wrapAsync(async (req, res) => {
+app.put(
+  "/listings/:id",
+  ValidateListing,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const list = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-}));
+  })
+);
 
 //Delete route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-}));
+  })
+);
 
 //review route
-app.post("/listings/:id/reviews", async (req, res) => {
+app.post(
+  "/listings/:id/reviews",
+  ValidateReview,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -102,22 +136,22 @@ app.post("/listings/:id/reviews", async (req, res) => {
     await newReview.save();
 
     console.log("review succesfully added");
-    res.redirect("/listings");
-});
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not found"));
+  next(new ExpressError(404, "Page not found"));
 });
 
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong" } = err;
-    // res.status(statusCode).send(message);
-    res.status(statusCode).render("error.ejs", { err });
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { err });
 });
 
-
 app.listen(8080, () => {
-    console.log("Server is listening to port 8080");
+  console.log("Server is listening to port 8080");
 });
 
 // app.get("/testListing", async (req, res) => {
--- models/review.js
+++ models/review.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let reviewSchema = new Schema({
-    comment: String,
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const Review = mongoose.model("Review", reviewSchema);
